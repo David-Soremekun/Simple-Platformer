7fe08740293fab5c622616f257b91eeba0f1f174
David S <b1053474@live.tees.ac.uk>
2023-08-27 00:48:37 +0100

    Edited Powerup

--- TestPlatformer/Assets/Scripts/Powerups.cs
+++ TestPlatformer/Assets/Scripts/Powerups.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93d647cb9f4c5e2ad4aa8a5c398b63bf8b1723485da7e5ee0da76a6bb8f0ac5a
-size 879
+oid sha256:58159d7c0644b9699610e0bf92da62525e6e7451288c33f0e10a9cd0081e1811
+size 947
